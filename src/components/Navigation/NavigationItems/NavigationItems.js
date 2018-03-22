1248a80fc94b8293f866e989da70aff3835dcdbf
Dylan Yung <YungDexiong@hotmail.com>
2018-03-22 03:06:06 -0700

    Added hamburger icon

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,12 +3,12 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
-    <ul className={classes.navigationItems}>
-        <NavigationItem link="/" active>Events</NavigationItem>
-        <NavigationItem link="/" active>Leaderboards</NavigationItem>
-        <NavigationItem link="/" active>Reserve Table</NavigationItem>
-        <NavigationItem link="/" active>Sign Out</NavigationItem>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/">Events</NavigationItem>
+        <NavigationItem link="/">Leaderboards</NavigationItem>
+        <NavigationItem link="/">Reserve Table</NavigationItem>
+        <NavigationItem link="/">Sign Out</NavigationItem>
     </ul>
 );
-//!!!TODO link should go to login page
+//!!!TODO link should go to respective pages
 export default navigationItems;
