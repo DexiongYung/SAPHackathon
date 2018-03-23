6c510b024d471c03bb396c8ca964a43b5883f655
Dylan Yung <YungDexiong@hotmail.com>
2018-03-22 21:17:29 -0700

    live update added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,10 +4,10 @@ import { NavigationItem } from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem onClick={props.onClick}>Profile</NavigationItem>
+        <NavigationItem onClick={props.onClick}>Events</NavigationItem>
         <NavigationItem onClick={props.onClick}>Meet Up</NavigationItem>
         <NavigationItem onClick={props.onClick}>Leaderboard</NavigationItem>
-        <NavigationItem onClick={props.onClick}>Events</NavigationItem>
+        <NavigationItem onClick={props.onClick}>Current Reservations</NavigationItem>
         <NavigationItem onClick={props.onClick}>Sign Out</NavigationItem>
     </ul>
 );
